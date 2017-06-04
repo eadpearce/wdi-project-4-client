b4c86c5723c7953e42b69e42a3219c92d0aeb808
eadpearce <e.pearce@tutanota.com>
2017-06-04 16:19:29 +0100

    Added search options for prompts

--- src/js/controllers/prompts.controller.js
+++ src/js/controllers/prompts.controller.js
@@ -4,12 +4,57 @@ angular
   .controller('PromptsShowCtrl', PromptsShowCtrl)
   .controller('PromptsNewCtrl', PromptsNewCtrl)
   .controller('PromptsIndexTagCtrl', PromptsIndexTagCtrl)
-  .controller('PromptsIndexUserCtrl', PromptsIndexUserCtrl);
+  .controller('PromptsIndexUserCtrl', PromptsIndexUserCtrl)
+  .filter('filterByTags', filterByTags);
 
-PromptsIndexCtrl.$inject = ['Prompt'];
-function PromptsIndexCtrl(Prompt) {
+filterByTags.$inject = [];
+function filterByTags() {
+  return function (items, q) {
+    const filtered = [];
+    let tagFound;
+    items.forEach(item => {
+      item.tags.forEach(tag => {
+        tagFound = tag.name === q;
+      });
+      if (tagFound) filtered.push(item);
+    });
+    return filtered;
+  };
+}
+
+PromptsIndexCtrl.$inject = ['Prompt', 'filterFilter', '$scope'];
+function PromptsIndexCtrl(Prompt, filterFilter, $scope) {
   const vm = this;
+  vm.limit = 10;
+  vm.clearSearch = function clearSearch() {
+    if (!vm.clearedSearch) vm.clearedSearch = true;
+    else vm.clearedSearch = false;
+  };
+  vm.isFilteredByFilled = false;
+  vm.isFilteredByUnfilled = false;
   vm.all = Prompt.query();
+  vm.showSearchOpts = false;
+  vm.searchOptions = function searchOptions() {
+    if (vm.showSearchOpts) vm.showSearchOpts = false;
+    else vm.showSearchOpts = true;
+  };
+
+  function filterPrompts() {
+    const params = { title: vm.q };
+    // if (vm.useStrength) params.strength = vm.strength;
+    // if (vm.useRoast) params.roast = vm.roast;
+    vm.filtered = filterFilter(vm.all, params);
+  }
+
+  // filterCoffee();
+  // watch multiple different values
+  $scope.$watchGroup([
+    () => vm.q
+    // () => vm.strength,
+    // () => vm.useStrength,
+    // () => vm.roast,
+    // () => vm.useRoast
+  ], filterPrompts);
 }
 
 PromptsNewCtrl.$inject = ['Prompt', '$state'];
